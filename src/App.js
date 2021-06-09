a4d5f8762420c64f55b1d519cdd9fa7c5aba4829
ZMAN <etta1055@gmail.com>
2021-06-09 17:35:36 -0500

    added css for app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const api = {
+  key: "568ada73d42b03c55773847f98ad37c5",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     <main>
+       <div className="search-box">
+         <input type="text"
+         className="search-bar"
+         placeholder="Search..."
+         />
+       </div>
+     </main>
     </div>
   );
 }
